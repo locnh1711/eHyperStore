03821d3e7342900cd6c683fa26b5ac9368015351
Loc Nguyen Hieu <locnh1711@gmail.com>
2021-08-31 14:10:17 +0700

    Configure entity with Fluent API and Migration database

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 ## Technologies
 - ASP.NET Core 3.3
 - Entity Framework Core 3.1
+## Install Packages
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.SqlServer
 ## How to configure and run
 ## How to contribute
---
+++ eHyperStore.Data/EF/EHyperDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eHyperStore.Data.EF
+{
+    internal class EHyperDbContextFactory : IDesignTimeDbContextFactory<EHyperDbContext>
+    {
+        public EHyperDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("eHyperStoreDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EHyperDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EHyperDbContext(optionsBuilder.Options);
+        }
+    }
+}
