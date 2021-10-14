9b6083883451b20950232493da59918e802ae1c4
Loc Nguyen Hieu <locnh1711@gmail.com>
2021-10-15 00:52:58 +0700

    Product category and detail

--- eHyperStore.BackendApi/Controllers/HomeController.cs
+++ eHyperStore.BackendApi/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace eHyperStore.BackendApi.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return Ok();
         }
 
         public IActionResult Privacy()
