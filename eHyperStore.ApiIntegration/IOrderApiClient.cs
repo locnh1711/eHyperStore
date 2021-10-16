bd13012aa147947c1c91cf815256b345853594eb
Loc Nguyen Hieu <locnh1711@gmail.com>
2021-10-16 20:57:35 +0700

    Checkout the order

---
+++ eHyperStore.ApiIntegration/IOrderApiClient.cs
@@ -0,0 +1,11 @@
+﻿using eHyperStore.ViewModels.Common;
+using eHyperStore.ViewModels.System.Languages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eHyperStore.ApiIntegration
+{
+    public interface IOrderApiClient
+    {
+    }
+}
